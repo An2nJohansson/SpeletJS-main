92eaa74da679874ffa039894025c27d1c78b6792
An2nJohansson <an2n_johansson@outlook.com>
2024-12-25 22:30:56 +0100

    update

--- SpeletJS-main/scripts.js
+++ SpeletJS-main/scripts.js
@@ -3,6 +3,99 @@ const timerDisplay = document.getElementById("timer-display");
 const allCards = document.querySelectorAll(".card");
 const restartButton = document.getElementById("restart");
 const attemptsCounter = document.querySelector(".flips b");
+const cardsContainer = document.querySelector(".cards");
+
+//kort
+const imgArray = [
+    "img/img1.jpg", "img/img1.jpg",
+    "img/img2.jpg", "img/img2.jpg",
+    "img/img3.jpg", "img/img3.jpg",
+    "img/img4.jpg", "img/img4.jpg",
+    "img/img5.jpg", "img/img5.jpg",
+    "img/img6.jpg", "img/img6.jpg",
+];
+
+let flippedCards = [];
+let matchedCards = [];
+let attempts = 0
+
+function shuffle(array) {
+    return array.sort(() => Math.random() - 0.5);
+}
+
+function cardGenerator() {
+    const shuffledCards = shuffle([...imgArray]);
+    cardsContainer.innerHTML = "";
+
+    shuffledCards.forEach((img) => {
+        const card = document.createElement("li");
+        card.classList.add("card");
+        card.innerHTML = `
+            <div class="view front-view">
+                <img class="img-front" src="img/qmark.png" alt="icon">
+            </div>
+            <div class="view back-view">
+                <img class="img-back" src="${img}" alt="card-img">
+            </div>
+        `;
+        cardsContainer.appendChild(card);
+    });
+    clickCard();
+}
+
+
+function clickCard() {
+    const allCards = document.querySelectorAll(".card");
+    allCards.forEach((card) => {
+        card.addEventListener("click", () => {
+            flipCard(card);
+        });
+    });
+}
+
+function flipCard(card) {
+    if (flippedCards.length === 0) {
+        countdownStart();
+    }
+    
+    if (flippedCards.length < 2 && !card.classList.contains("flip") && !matchedCards.includes(card)) {
+        card.classList.add("flip");
+        flippedCards.push(card);
+
+        if (flippedCards.length === 2) {
+            checkMatch();
+        }
+    }
+}
+
+function checkMatch() {
+    const [card1, card2] = flippedCards;
+
+    if (card1.querySelector(".back-view img").src === card2.querySelector(".back-view img").src) {
+        matchedCards.push(card1, card2);
+        flippedCards = [];
+        attempts++;
+        usedAttempts();
+
+        if (matchedCards.length === imgArray.length) {
+            alert("Du vann!");
+        }
+    }
+
+    else {
+        setTimeout(() => {
+            card1.classList.remove("flip");
+            card2.classList.remove("flip");
+            flippedCards = [];
+            attempts++;
+            usedAttempts();
+        }, 1000);
+    }
+}
+
+function usedAttempts() {
+    attemptsCounter.textContent = attempts;
+}
 
 //tid
 selectTime.addEventListener("change", () => {
@@ -32,43 +125,24 @@ function countdownStart() {
     }, 1000);
 }
 
-//antal försök
-let attempts = 0;
-function usedAttempts() {
-    attempts++;
-    attemptsCounter.textContent = attempts;
-}
-
-//klicka på kort
-allCards.forEach((card) => {
-    card.addEventListener("click", () => {
-        countdownStart();
-        card.classList.toggle("flip");
-        console.log("Kort vänds:", card);
-
-        const flippedCards = document.querySelectorAll(".card.flip");
-        if (flippedCards.length === 2) {
-            usedAttempts();
-        }
-    });
-});
-
 //restart button
 restart.addEventListener("click", () => {
+    cardGenerator();
     allCards.forEach((card) => {
         card.classList.remove("flip");
     });
 
+    cardGenerator();
+    matchedCards = [];
+    flippedCards = [];
+    attempts = 0;
+    usedAttempts();
     clearInterval(countDown);
     timerRunning = false;
     selectTime.disabled = false;
     selectTime.style.display = "block";
-    
     const resetTime = parseInt(selectTime.value);
     timerDisplay.textContent = `Tid:`;
-
-    attempts = 0;
-    attemptsCounter.textContent = attempts;
-
-    document.querySelector(".flips span b").textContent = 0;
 });
+
+cardGenerator();
